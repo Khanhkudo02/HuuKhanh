1125ae12f3098cba9d33397f7b4d6ee9ee72e521
Khanhkudo02 <113323275+Khanhkudo02@users.noreply.github.com>
2026-06-14 23:36:49 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,20 +1,46 @@
 function toggleSidebar(){
-document
-.getElementById("sidebar")
-.classList.toggle("show");
+    document.getElementById("sidebar")
+    .classList.toggle("show");
 }
 
 async function loadPage(page){
 
-const res = await fetch(page);
+    const content =
+    document.getElementById("content");
 
-const html = await res.text();
+    try{
 
-document.getElementById("content").innerHTML = html;
+        const res = await fetch(page);
 
-if(window.innerWidth < 768){
-document
-.getElementById("sidebar")
-.classList.remove("show");
-}
+        const html = await res.text();
+
+        content.innerHTML = html;
+
+        // chạy lại script trong file html được load
+
+        const scripts =
+        content.querySelectorAll("script");
+
+        scripts.forEach(oldScript=>{
+
+            const newScript =
+            document.createElement("script");
+
+            newScript.textContent =
+            oldScript.textContent;
+
+            document.body.appendChild(newScript);
+
+            oldScript.remove();
+
+        });
+
+    }catch(err){
+
+        content.innerHTML=
+        "<h2>Lỗi tải dữ liệu</h2>";
+
+        console.error(err);
+
+    }
 }
